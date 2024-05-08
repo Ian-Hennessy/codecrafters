7c43349480b8c4d5dc0d4ce2ec2a2198bbedeb59
Ian-Hennessy <ihennessy3@gatech.edu>
2024-05-08 12:40:38 -0500

    HTTPS socket connect and return message test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,41 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    //System.out.println("Logs from your program will appear here!");
+    static String CRLF = "\r\n";
 
-    // Uncomment this block to pass the first stage
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        //System.out.println("Logs from your program will appear here!");
 
-     ServerSocket serverSocket = null;
-     Socket clientSocket = null;
+        // Uncomment this block to pass the first stage
 
-     try {
-       serverSocket = new ServerSocket(4221);
-       serverSocket.setReuseAddress(true);
-       clientSocket = serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+
+        try {
+//       serverSocket = new ServerSocket(4221);
+//       serverSocket.setReuseAddress(true);
+//       clientSocket = serverSocket.accept(); // Wait for connection from client.
+//       System.out.println("accepted new connection");
+            try {
+                serverSocket = new ServerSocket(4221);
+                try {
+                    clientSocket = serverSocket.accept();
+                    OutputStream clientOutput = clientSocket.getOutputStream();
+                    BufferedWriter bufferedWriter =
+                            new BufferedWriter(new OutputStreamWriter(clientOutput));
+                    bufferedWriter.write("HTTP/1.1 200 OK" + CRLF + CRLF);
+                    bufferedWriter.close();
+                } catch (IOException e) {
+                    System.out.println("IOExceltion:" + e.getMessage());
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
