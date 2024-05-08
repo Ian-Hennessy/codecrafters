745dd1ceecd1ebb311fa04d5fc1ec3989ac2224d
Ian-Hennessy <ihennessy3@gatech.edu>
2024-05-08 16:13:40 -0500

    Feature to handle multiple processed requests at one time.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,57 +4,74 @@ import java.net.Socket;
 import java.util.Objects;
 
 public class Main {
-    static String CRLF = "\r\n";
+    private Socket clientSocket;
 
-    public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        //System.out.println("Logs from your program will appear here!");
+    public void clientHandler(Socket client) {
+        this.clientSocket = client;
+    }
+    String CRLF = "\r\n";
 
-        // Uncomment this block to pass the first stage
-
-        //       serverSocket = new ServerSocket(4221);
-//       serverSocket.setReuseAddress(true);
-//       clientSocket = serverSocket.accept(); // Wait for connection from client.
-//       System.out.println("accepted new connection");
-        try (ServerSocket serverSocket = new ServerSocket(4221)) {
-            try (Socket clientSocket = serverSocket.accept()) {
-                OutputStream clientOutput = clientSocket.getOutputStream();
-                InputStream clientInput = clientSocket.getInputStream();
-                BufferedWriter bufferedWriter =
-                        new BufferedWriter(new OutputStreamWriter(clientOutput));
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(clientInput));
-                String firstLine = bufferedReader.readLine();
-                String path = firstLine.split(" ")[1];
-                if (path.equals("/")) {
-                    bufferedWriter.write("HTTP/1.1 200 OK" + CRLF + CRLF);
-                } else if (path.split("/")[1].equals("echo")) {
-                        String txt = path.split("/")[2];
-                        String response =
-                                "HTTP/1.1 200 OK" + CRLF + "Content-Type: text/plain" +
-                                        CRLF + "Content-Length: " + txt.length() +
-                                        CRLF + CRLF + txt;
-                        bufferedWriter.write(response);
-                    } else if (path.split("/")[1].equals("user-agent")) {
-                    String host = bufferedReader.readLine();
-                    String agentLine = bufferedReader.readLine();
-                    if (agentLine.split(" ")[0].equals("User-Agent:")) {
-                        String content = agentLine.split(" ")[1];
-                        String response =
-                                "HTTP/1.1 200 OK" + CRLF + "Content-Type: text/plain" +
-                                        CRLF + "Content-Length: " + content.length() +
-                                CRLF + CRLF + content;
-                        bufferedWriter.write(response);
-                    }
-                } else {
-                    bufferedWriter.write("HTTP/1.1 404 NOT FOUND" + CRLF + CRLF);
-                }
-                bufferedWriter.close();
-            } catch (IOException e) {
-                System.out.println("IOException:" + e.getMessage());
-            }
-        } catch (IOException e) {
+    public void run() {
+        try {
+            process();
+        } catch (IOException e){
             System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException ex) {
+                System.out.println("IOException: " + ex.getMessage());
+            }
         }
     }
+
+    private void process() throws IOException{
+
+        OutputStream clientOutput = clientSocket.getOutputStream();
+        InputStream clientInput = clientSocket.getInputStream();
+        BufferedWriter bufferedWriter =
+                new BufferedWriter(new OutputStreamWriter(clientOutput));
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(clientInput));
+        String firstLine = bufferedReader.readLine();
+        String path = firstLine.split(" ")[1];
+
+        if (path.equals("/")) {
+
+            bufferedWriter.write("HTTP/1.1 200 OK" + CRLF + CRLF);
+
+        } else if (path.split("/")[1].equals("echo")) {
+
+                String txt = path.split("/")[2];
+                String response =
+                        "HTTP/1.1 200 OK" + CRLF + "Content-Type: text/plain" +
+                                CRLF + "Content-Length: " + txt.length() +
+                                CRLF + CRLF + txt;
+                bufferedWriter.write(response);
+
+            } else if (path.split("/")[1].equals("user-agent")) {
+
+            String host = bufferedReader.readLine();
+            String agentLine = bufferedReader.readLine();
+
+            if (agentLine.split(" ")[0].equals("User-Agent:")) {
+
+                String content = agentLine.split(" ")[1];
+                String response =
+                        "HTTP/1.1 200 OK" + CRLF + "Content-Type: text/plain" +
+                                CRLF + "Content-Length: " + content.length() +
+                        CRLF + CRLF + content;
+                bufferedWriter.write(response);
+
+            }
+        } else {
+
+            bufferedWriter.write("HTTP/1.1 404 NOT FOUND" + CRLF + CRLF);
+
+        }
+        bufferedWriter.close();
+
+    }
+
+
 }
