e87283c852ba1f16fadd8293137edab876f2e11e
Ian-Hennessy <ihennessy3@gatech.edu>
2024-05-09 10:11:18 -0500

    Added support for multiple requests via the clientHandler class.

--- src/main/java/clientHandler.java
+++ src/main/java/clientHandler.java
@@ -35,37 +35,34 @@ public class clientHandler implements Runnable {
 
         String firstLine = clientReader.readLine();
         String path = firstLine.split(" ")[1];
-        switch (path) {
-            case "/" -> {
-                String response = "HTTP/1.1 200 OK" + CRLF + CRLF;
-                clientOutputStream.write(response.getBytes());
-            }
-            case "/echo" -> {
-                String txt = path.split("/")[1];
-                String response = "HTTP/1.1 200 OK" + CRLF +
-                        "Content-Type: text/plain" + CRLF +
-                        "Content-Length: " + txt.length() + CRLF + CRLF + txt;
-                clientWriter.write(response);
-            }
-            case "/user-agent" -> {
-                String headerPair = clientReader.readLine();
-                while (headerPair != null) {
-                    String[] split = headerPair.split(":");
-                    String val = split[1].strip();
-                    if (split[0].equals("User-Agent")) {
-                        String response = "HTTP/1.1 200 OK" + CRLF +
-                                "Content-Type: text/plain" + CRLF +
-                                "Content-Length: " + val.length() +
-                                CRLF + CRLF + val;
-                        System.out.println(response);
-                        clientOutputStream.write(response.getBytes(StandardCharsets.US_ASCII));
-                    }
-                    headerPair = clientReader.readLine();
+
+        if (path.equals("/")) {
+            String response = "HTTP/1.1 200 OK" + CRLF + CRLF;
+            clientOutputStream.write(response.getBytes());
+        } else if (path.split("/")[0].equals("echo")) {
+            String txt = path.split("/")[1];
+            String response = "HTTP/1.1 200 OK" + CRLF +
+                    "Content-Type: text/plain" + CRLF +
+                    "Content-Length: " + txt.length() + CRLF + CRLF + txt;
+            clientWriter.write(response);
+        } else if (path.equals("/user-agent")) {
+            String headerPair = clientReader.readLine();
+            while (headerPair != null) {
+                String[] split = headerPair.split(":");
+                String val = split[1].strip();
+                if (split[0].equals("User-Agent")) {
+                    String response = "HTTP/1.1 200 OK" + CRLF +
+                            "Content-Type: text/plain" + CRLF +
+                            "Content-Length: " + val.length() +
+                            CRLF + CRLF + val;
+                    System.out.println(response);
+                    clientOutputStream.write(response.getBytes(StandardCharsets.US_ASCII));
                 }
+                headerPair = clientReader.readLine();
             }
-            default -> clientWriter.write("HTTP/1.1 404 NOT FOUND"  + CRLF + CRLF);
+        } else {
+            clientWriter.write("HTTP/1.1 404 NOT FOUND" + CRLF + CRLF);
         }
         clientWriter.close();
     }
-
 }
